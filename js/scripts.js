72c7c43fc1894a0f788a076c606987631ca2c01b
Your Name <nntwari75@gmail.com>
2021-01-10 14:07:31 -0800

    adding js codes

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$("#submit_btn").addEventListener("click",function(){
+	alert("Thank you for reaching to us")
+});
+$("#button-order").addEventListener("click",function(){
+	var number;toppings;size;crust;total;
+
+	crust=document.getElementById("crust").value;
+	size=document.getElementById("size").value;
+	toppings=document.getElementById("toppings").value;
+	number=document.getElementById("number").value;
+
+});
